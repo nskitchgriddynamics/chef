0827bb00cdb0a0fb14587ec5162afb6e56814b1b
nskitch <nskitch@griddynamics.com>
2015-06-29 18:04:52 -0700

    chef 6pm progress checkin -- install zabbix_get & UserParam to zabbix_agentd.conf

---
+++ cookbooks/zabbix/attributes/zabbix.rb
@@ -0,0 +1,6 @@
+default['zabbix']['config_file'] = '/etc/zabbix/zabbix_agentd.conf'
+default['zabbix']['user_param1'] = 'UserParameter=nickTest.nickCount,ls -l /etc/zabbix/nickTemp/ | wc -l'
+default['zabbix']['script_dir'] = '/etc/zabbix/nickTemp/'
+
+
+   
--- cookbooks/zabbix/recipes/default.rb
+++ cookbooks/zabbix/recipes/default.rb
@@ -8,6 +8,25 @@ yum_package "zabbix-agent" do
 	action :install
 end
 
+
+yum_package "zabbix-get" do 
+	action :install
+end
+
+# this will hold file counts
+directory "#{node[:zabbix][:script_dir]}" do 
+  recursive true
+end
+
+template "#{node[:zabbix][:config_file]}" do
+  path "#{node[:zabbix][:config_file]}"
+  source "zabbix_agentd.conf.erb"
+  owner "zabbix"
+  group "zabbix"
+  mode "0750"
+end
+
 service "zabbix-agent" do
 	action [:enable, :start]
 end
+
